ad39ed4a6f098332a4f63e0967c15bbdc289d9aa
Aleksei Vavulo <92637953+AlexLancev@users.noreply.github.com>
2022-10-24 08:21:59 +0500

    Add files via upload

--- js/modal.js
+++ js/modal.js
@@ -1,120 +1,118 @@
-  const popupLinks = document.querySelectorAll('.popup-link');
-  const body = document.querySelector('body');
-  const lockPadding = document.querySelectorAll('.lock-padding');
-  const active = document.querySelector('.nav__list-content');
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
 
-  let unlock = true;
-  const timeout = 500;
+let unlock = true;
 
-  if (popupLinks.length > 0) {
+const timeout = 800;
+
+if(popupLinks.length > 0) {
     for (let index = 0; index < popupLinks.length; index++) {
-      const popupLink = popupLinks[index];
-      popupLink.addEventListener('click', function (e) {
-        const popupName = popupLink.getAttribute('href').replace('#', '');
-        const curentPopup = document.getElementById(popupName);
-        popupOpen(curentPopup);
-        e.preventDefault();
-      });
+        const popupLink = popupLinks[index];
+        popupLink.addEventListener('click', function(e) {
+            const popupName = popupLink.getAttribute('data-href').replace('#', '');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault();
+        });
     }
-  }
-  const popupCloseIcon = document.querySelectorAll('.close-popup');
-  if (popupCloseIcon.length > 0) {
+}
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
     for (let index = 0; index < popupCloseIcon.length; index++) {
-      const el = popupCloseIcon[index];
-      el.addEventListener('click', function (e) {
-        popupClose(el.closest('.popup'));
-        e.preventDefault();
-      })
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        })
     }
-  }
+}
 
-  function popupOpen(curentPopup) {
+function popupOpen(curentPopup) {
     if (curentPopup && unlock) {
-      const popupActive = document.querySelector('.popup.open');
-      if (popupActive) {
-        popupClose(popupActive, false);
-      } else {
-        bodyLock();
-      }
-
-      curentPopup.classList.add('open');
-      curentPopup.addEventListener('click', function (e) {
-        if (!e.target.closest('.popup__content')) {
-          popupClose(e.target.closest('.popup'));
+        const popupActive = document.querySelector('.popup.open');
+        if (popupActive) {
+            popupClose(popupActive, false);
+        } else {
+            bodyLock();
         }
-      });
+        curentPopup.classList.add('open');
+        curentPopup.addEventListener('click', function (e) {
+            if(!e.target.closest('.popup__content')) {
+                popupClose(e.target.closest('.popup'));
+            }
+        });
     }
-  }
-
-  function popupClose(popupActive, doUnlock = true) {
+}
+function popupClose(popupActive, doUnlock = true) {
     if (unlock) {
-      popupActive.classList.remove('open');
-      if (doUnlock) {
-        bodyUnLock();
-      }
+        popupActive.classList.remove('open');
+        if (doUnlock) {
+            bodyUnLock();
+        }
     }
-  }
+}
 
-  function bodyLock() {
+function bodyLock() {
     const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
 
     if (lockPadding.length > 0) {
-      for (let index = 0; index < lockPadding.length; index++) {
-        const el = lockPadding[index];
-        el.style.paddingRight = lockPaddingValue;
-      }
-    }
-      body.style.paddingRight = lockPaddingValue;
-      body.classList.add('lock');
-
-    unlock = false;
-    setTimeout(function () {
-      unlock = true;
-    }, timeout);
-  }
-
-  function bodyUnLock() {
-    setTimeout(function () {
-      if (lockPadding.lenght > 0) {
-        for (let index = 0; index < lockPadding.length; index++) {
-          const el = lockPadding[index];
-          el.style.paddingRight = '0px';
+        for(let index = 0; index < lockPadding.length; index++) {
+            const el = lockPadding[index];
+            el.style.paddingRight = lockPaddingValue;
         }
-      }
-      body.style.paddingRight = '0px';
-      body.classList.remove('lock');
-    }, timeout);
+    }
+    body.style.paddingRight = lockPaddingValue;
+    body.classList.add('lock');
+
     unlock = false;
     setTimeout(function () {
-      unlock = true;
-    }, timeout);
-  }
+        unlock = true;
+    },  timeout);
+}
 
-  document.addEventListener('keydown', function (e) {
+function bodyUnLock() {
+    setTimeout(function () {
+        if (lockPadding.lenght > 0) {
+            for(let index = 0; index < lockPadding.length; index++) {
+                const el = lockPadding[index];
+                el.style.paddingRight = '1px';
+            }
+        }
+        body.style.paddingRight = '1px';
+        body.classList.remove('lock');
+     }, timeout);
+
+     unlock = false;
+     setTimeout(function () {
+        unlock = true;
+     }, timeout);
+}
+
+document.addEventListener('keydown', function(e) {
     if (e.which === 27) {
-      const popupActive = document.querySelector('.popup.open');
-      popupClose(popupActive);
+        const popupActive = document.querySelector('.popup.open');
+        popupClose(popupActive);
     }
-  });
+});
 
-  (function () {
+(function() {
     if (!Element.prototype.closest) {
-      Element.prototype.closest = function (css) {
-        var node = this;
-        while (node) {
-          if (node.matches(css)) return node;
-          else node = node.parentElement;
-        }
-        return null;
-      };
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
     }
-  })();
-
-  (function () {
-    if (!Element.prototype.matches) {
-      Element.prototype.matches = Element.prototype.matchesSelector ||
-        Element.prototype.webkitMatchesSelector ||
-        Element.prototype.mozMathesSelector ||
-        Element.prototype.msMatchesSelector;
+})();
+(function () {
+    if(!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+          Element.prototype.webkitMatchesSelector ||
+          Element.prototype.mozMathesSelector ||
+          Element.prototype.msMatchesSelector;
     }
-  })();
+})();
